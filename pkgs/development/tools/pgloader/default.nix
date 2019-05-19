c2f6a7d861a7e463e4f0fa5ede87381a3e147b0a
Maximilian Güntner <code@sourcediver.org>
2019-05-19 12:04:42 +0200

    pgloader: build using src, bundle in own derivation
    does not work because the derivation that fetches
    the dependencies is not reproducible

--- pkgs/development/tools/pgloader/default.nix
+++ pkgs/development/tools/pgloader/default.nix
@@ -1,13 +1,45 @@
 { stdenv, fetchurl, makeWrapper, sbcl, sqlite, freetds, libzip, curl, git, cacert, openssl }:
-stdenv.mkDerivation rec {
+let
   pname = "pgloader";
   version = "3.6.1";
   name = "${pname}-${version}";
 
   src = fetchurl {
-    url = "https://github.com/dimitri/pgloader/releases/download/v3.6.1/pgloader-bundle-3.6.1.tgz";
-    sha256 = "1sm8xmq30d1biin5br0y3vrv4fydbrzfqglz1hnvrkdyxrg7d6f9";
+    url = "https://github.com/dimitri/pgloader/archive/v${version}.tar.gz";
+    sha256 = "1yh9ifwiysm0hhyppdxkhyyr4w51jwdvc0hcf3vwb54wiqp4zabg";
   };
+
+  deps = stdenv.mkDerivation {
+    name = "${name}-deps";
+    inherit src;
+    buildInputs = [ sbcl cacert sqlite freetds libzip curl git openssl ];
+
+    LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [ sqlite libzip curl git openssl freetds ];
+
+    buildPhase = ''
+      export PATH=$PATH:$out/bin
+      export HOME=$TMPDIR
+
+      make libs
+      make buildapp
+    '';
+
+    installPhase = ''
+      cp -a . $out
+    '';
+
+    dontStrip = true;
+    enableParallelBuilding = false;
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = "0gwjhybwnijdf4279ic1jshg2dqlidlsgpbngmlwajfg8apkij7z";
+  };
+
+in
+stdenv.mkDerivation rec {
+
+  inherit name pname deps;
 
   buildInputs = [ sbcl cacert sqlite freetds libzip curl git openssl makeWrapper ];
 
@@ -20,12 +52,22 @@ stdenv.mkDerivation rec {
     make pgloader
   '';
 
+  src = deps;
+
   dontStrip = true;
   enableParallelBuilding = false;
 
   installPhase = ''
-    install -Dm755 bin/pgloader "$out/bin/pgloader"
+    install -Dm755 build/bin/pgloader "$out/bin/pgloader"
     wrapProgram $out/bin/pgloader --prefix LD_LIBRARY_PATH : "${LD_LIBRARY_PATH}"
   '';
+
+  meta = with stdenv.lib; {
+    homepage = https://pgloader.io/;
+    description = "pgloader loads data into PostgreSQL and allows you to implement Continuous Migration from your current database to PostgreSQL";
+    maintainers = with maintainers; [ mguentner ];
+    license = licenses.postgresql;
+    platforms = platforms.all;
+  };
 
 }
